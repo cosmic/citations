44b20c5532a031280aeca178d1c5069bddd87e3d
Sunny Ripert <sunny@sunfox.org>
2013-05-06 19:46:26 +0200

    PHP short tags

--- index.php
+++ index.php
@@ -1,38 +1,35 @@
-<? $file = file_get_contents('../edith/data/citations.txt'); ?>
 <?
-
 function markup($text) {
   $html = htmlspecialchars($text);
-  $html = preg_replace("`(http|ftp)+(s)?:(//)((\w|\.|\-|_)+)(/)?(\S+)?`i", "<a href=\"\\0\">\\0</a>", $html);
-  return $html;
+  return preg_replace("`(http|ftp)+(s)?:(//)((\w|\.|\-|_)+)(/)?(\S+)?`i", "<a href=\"\\0\">\\0</a>", $html);
 }
-
+$file = file_get_contents('../edith/data/citations.txt');
 ?>
 
 <!DOCTYPE html>
 <head>
   <meta charset="utf-8">
   <title>Citations</title>
-  <link href='//fonts.googleapis.com/css?family=Noto+Serif' rel='stylesheet' />
+  <link href="//fonts.googleapis.com/css?family=Noto+Serif" rel="stylesheet" />
   <link rel="stylesheet" href="css/citations.css" />
 </head>
 <body>
   <h1>Citations</h1>
 
-<?php foreach (explode("\n\n", $file) as $block) : ?>
+<? foreach (explode("\n\n", $file) as $block) : ?>
   <ul>
-  <?php foreach (explode("\n", $block) as $line) : ?>
-    <?php if ($line and list($user, $line) = preg_split('/: ?/', $line, 2)) : ?>
+  <? foreach (explode("\n", $block) as $line) : ?>
+    <? if ($line and list($user, $line) = preg_split('/: ?/', $line, 2)) : ?>
 
       <li>
         <strong><?= htmlspecialchars($user) ?></strong>&nbsp;:
         <?= markup($line) ?>
       </li>
-    <?php endif; ?>
-  <?php endforeach; ?>
+    <? endif; ?>
+  <? endforeach; ?>
 
   </ul>
-<?php endforeach; ?>
+<? endforeach; ?>
 
 
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
@@ -40,7 +37,7 @@ function markup($text) {
   <script src="scripts/application.js"></script>
 </body>
 </html>
-<?php // Code is poetry ?>
+<? // Code is poetry ?>
 
 <!--   |\/|
       bd "n.
